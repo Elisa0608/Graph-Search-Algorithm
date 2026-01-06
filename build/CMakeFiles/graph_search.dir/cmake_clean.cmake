d6d496dfed584c0a3225f7fe181ce426cc28fd6c
Elisa0608 <elisamosoarca@gmail.com>
2026-01-06 15:41:50 +0200

    Wrote the CMake file

---
+++ build/CMakeFiles/graph_search.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph_search.dir/main.cpp.obj"
+  "CMakeFiles/graph_search.dir/main.cpp.obj.d"
+  "CMakeFiles/graph_search.dir/src/Algorithms.cpp.obj"
+  "CMakeFiles/graph_search.dir/src/Algorithms.cpp.obj.d"
+  "CMakeFiles/graph_search.dir/src/Graph.cpp.obj"
+  "CMakeFiles/graph_search.dir/src/Graph.cpp.obj.d"
+  "graph_search.exe"
+  "graph_search.exe.manifest"
+  "graph_search.pdb"
+  "libgraph_search.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

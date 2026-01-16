78a50cdc1b88f5b0eeb602dd5d70b6e9d0730eb0
Elisa0608 <elisamosoarca@gmail.com>
2026-01-16 09:09:44 +0200

    redefinied the Graph structure
    Added  the Edge structure
    Modifiest de AdjList using std::map

--- src/Graph.h
+++ src/Graph.h
@@ -7,20 +7,16 @@
 #include <string>
 #include <stack>
 #include <queue>
+#include <map>
 
-//cream un alias pt WE folosisnd un container ce stocheaza 2 val
-//in pair avem ddestinatia si ponderea
-using WeightedEdge = std::pair<int, int>;
+struct Edge{
+    std::string to;
+    int weight;
+};
 
 class Graph{
 private:
-    int nodes;
-    //vectorul de liste
-    std::vector<std::list<WeightedEdge>> adj;
-    //primeste un nod, starea visited si calea curenta
-    void DFSutil(int nodes, std::vector<bool>& visited, std::list<int>&path);
-    //primeste un nod, starea visited si stiva de recursivitate
-    bool isCyclicUtil(int nodes, std::vector<bool>& visited, std::vector<bool>& recursionStack);
+    std::map<std::string, std::vector<Edge>> adjList;
 public:
     Graph(int nodes);
 

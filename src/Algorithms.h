ad3647147cfb419fbacaef52a19ea7097f108f06
Elisa0608 <elisamosoarca@gmail.com>
2026-01-21 14:36:32 +0200

    Fixed BFS declaration
    Added the forgotten paramater to BFS declaration in Algorithms.h and included Graph.h in Algorithms.h
    BFS algorithm works now

--- src/Algorithms.h
+++ src/Algorithms.h
@@ -1,11 +1,12 @@
+#include "Graph.h"
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 #include <list>
 #include <string>
 
 namespace Algorithms {
-    std::list<std::string> BFS(const std::string& startNode, const std::string& targetNode = "");
-    std::list<std::string> DFS(const std::string& startNode, const std::string& targetNode = "");
+    std::list<std::string> BFS(const Graph& g, const std::string& startNode, const std::string& targetNode = "");
+    std::list<std::string> DFS(const Graph& g, const std::string& startNode, const std::string& targetNode = "");
 
 };
 
